6603f5466e18b8f2c5b2b3da6677c3f845c0042f
shinjh0729 <lemonhoney77@naver.com>
2021-05-09 17:54:55 +0900

    Creat React Project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{ Component } from 'react';
+import Customer from './components/Customer';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src = {logo} lat="logo"/>
-      <h2>Let's develop management system</h2>
-    </div>
-  );
+class App extends Component() {
+  render() {
+    return (
+      <Customer/>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Customer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Customer extends React.Component {
+    render() {
+        return(
+            <div>
+            <h2>신준혁</h2>
+            <p>970729</p>
+            <p>남자</p>
+            <p>대학생</p>
+            </div>
+        )
+    }
+}
+
+export default Customer;
