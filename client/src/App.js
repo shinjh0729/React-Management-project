cd60079f2e21f6bd959f6e9d51d2216d65ba98cc
shinjh0729 <lemonhoney77@naver.com>
2021-05-10 20:51:20 +0900

    ADD REST API

--- client/src/App.js
+++ client/src/App.js
@@ -19,34 +19,21 @@ const styles = theme => ({
     minWidth:1080
   }
 })
-const customers =[
-  {
-    'id' : 1,
-    'image' : './sjh.jpg',
-    'name' : '신준혁',
-    'birthday' : '970729',
-    'gender' : '남자',
-    'job' : '대학생'
-  },
-  {
-    'id' : 2,
-    'image' : './sjh.jpg',
-    'name' : '홍길동',
-    'birthday' : '111111',
-    'gender' : '남자',
-    'job' : '군인'
-  },
-  {
-    'id' : 3,
-    'image' : './sjh.jpg',
-    'name' : '김나박이',
-    'birthday' : '000000',
-    'gender' : '여자',
-    'job' : '가수'
-  }
-]
 
 class App extends Component {
+  state = {
+    customers: ""
+  }
+  componentDidMount() {
+    this.callApi()
+    .then(res => this.setState({customers: res}))
+    .catch(err => console.log(err));
+  }
+  callApi = async() =>{
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
   render(){
     const {classes } = this.props;
     return (
@@ -63,7 +50,8 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-          {customers.map(c=> {return( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />) })}
+          {this.state.customers ? this.state.customers.map(c=> {
+            return( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />) }) : ""}
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -6,7 +6,32 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:true}));
 
-app.get('/api/hello',(req, res) => {
-    res.send({message: 'Hello Express!'});
+app.get('/api/customers',(req, res) => {
+    res.send([
+        {
+            'id' : 1,
+            'image' : './sjh.jpg',
+            'name' : '신준혁',
+            'birthday' : '970729',
+            'gender' : '남자',
+            'job' : '대학생'
+          },
+          {
+            'id' : 2,
+            'image' : './sjh.jpg',
+            'name' : '홍길동',
+            'birthday' : '111111',
+            'gender' : '남자',
+            'job' : '군인'
+          },
+          {
+            'id' : 3,
+            'image' : './sjh.jpg',
+            'name' : '김나박이',
+            'birthday' : '000000',
+            'gender' : '여자',
+            'job' : '가수'
+          }
+    ]);
 });
 app.listen(port,() => console.log(`Listening on port ${port}`));
