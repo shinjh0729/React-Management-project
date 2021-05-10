6d1c7b2ea7b913e8d3a7df89f044470a1f086fe1
shinjh0729 <lemonhoney77@naver.com>
2021-05-10 13:41:15 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,55 @@ import './App.css';
 import React,{ Component } from 'react';
 import Customer from './components/Customer';
 
-function App(){
+const customers =[
+  {
+    'id' : 1,
+    'image' : './sjh.jpg',
+    'name' : '신준혁',
+    'birthday' : '970729',
+    'gender' : '남자',
+    'job' : '대학생'
+  },
+  {
+    'id' : 2,
+    'image' : './sjh.jpg',
+    'name' : '홍길동',
+    'birthday' : '111111',
+    'gender' : '남자',
+    'job' : '군인'
+  },
+  {
+    'id' : 3,
+    'image' : './sjh.jpg',
+    'name' : '김나박이',
+    'birthday' : '000000',
+    'gender' : '여자',
+    'job' : '가수'
+  }
+]
+
+class App extends Component {
+  render(){
     return (
-      <Customer/>
+      <div>
+        {
+          customers.map(c=> {
+            return(
+              <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+              />
+            )
+          })
+        }
+      </div>
     );
+  }
+    
 }
 export default App;
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -4,13 +4,32 @@ class Customer extends React.Component {
     render() {
         return(
             <div>
-            <h2>신준혁</h2>
-            <p>970729</p>
-            <p>남자</p>
-            <p>대학생</p>
+                <CustomerProfile id={this.props.id} image={this.props.image} name={this.props.name}/>
+                <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job}/>
             </div>
         )
     }
 }
 
+class CustomerProfile extends React.Component{
+    render(){
+        return(
+            <div>
+                <img src={this.props.image} alt="profile"/>
+                <h2>{this.props.name}({this.props.id})</h2>
+            </div>
+        )
+    }
+}
+class CustomerInfo extends React.Component{
+    render(){
+        return(
+            <div>
+                <p>{this.props.birthday}</p>
+                <p>{this.props.gender}</p>
+                <p>{this.props.job}</p>
+            </div>
+        )
+    }
+}
 export default Customer;
